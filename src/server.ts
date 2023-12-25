7e40bc7978a7004763f86c3a8a5fe45d642d76c4
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-25 17:57:30 -0300

    add: fastify routes plugin

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import fastify from "fastify"
 import knex from "knex"
 import { env } from "./env"
+import { transactionsRoutes } from "./routes/transactions"
 
 const app = fastify()
 
-app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*")
-
-  return tables
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
