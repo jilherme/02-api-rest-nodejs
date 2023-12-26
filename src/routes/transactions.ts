151535c1d8cf65649fedb9aeb6562de1baa413b8
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-25 22:25:39 -0300

    add: create and list transaction resources

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,44 @@
 import { FastifyInstance } from "fastify"
 import { knex } from "../database"
+import { randomUUID } from "crypto"
+import { z } from "zod"
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transactions = await knex("transactions").where("amount", 1000).select("*")
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select()
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get("/:id", async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex("transactions").where("id", id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
